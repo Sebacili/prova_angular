3e96d03dbb51222a7520eddb15963e5983aa6980
Cilibeanu Sebastian <cilibeanu.nicolae@iisgalvanimi.edu.it>
2022-09-23 08:56:08 +0000

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   angular-hello-world/src/app/app.component.html
    	modified:   angular-hello-world/src/app/app.component.ts
    	modified:   angular-hello-world/src/app/app.module.ts
    	new file:   angular-hello-world/src/app/user-item/user-item.component.css
    	new file:   angular-hello-world/src/app/user-item/user-item.component.html
    	new file:   angular-hello-world/src/app/user-item/user-item.component.spec.ts
    	new file:   angular-hello-world/src/app/user-item/user-item.component.ts
    	new file:   package-lock.json

--- angular-hello-world/src/app/app.module.ts
+++ angular-hello-world/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { UserItemComponent } from './user-item/user-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloWorldComponent
+    HelloWorldComponent,
+    UserItemComponent
   ],
   imports: [
     BrowserModule,
